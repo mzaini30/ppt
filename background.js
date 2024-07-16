00bbd990bc6ceb9fcfcc62f57e2a4cfee8f04b79
mzaini30 <muhzaini30@gmail.com>
2024-07-16 10:32:23 +0800

    upload

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.action.onClicked.addListener((tab) => {
   const newPrefix = "https://bukain.github.io/";
 
   if (tab.url.startsWith(githubPrefix)) {
-    const newUrl = tab.url.replace(githubPrefix, newPrefix);
+    const newUrl = tab.url.push(githubPrefix, newPrefix);
     chrome.tabs.update(tab.id, { url: newUrl });
   }
 });
